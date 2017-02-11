003bcd7cdad684db12e612bc15245f5d2ad4afd3
Raull2015 <raullfire@gmail.com>
2017-02-10 20:31:31 -0600

    endpoint del home creado

--- APIcasaCultura/apps/api/resources.py
+++ APIcasaCultura/apps/api/resources.py
@@ -1,27 +1,63 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, Resource
+from tastypie.serializers import Serializer
+from tastypie.authorization import DjangoAuthorization, ReadOnlyAuthorization
 from tastypie import fields
 from datetime import date
 from models import *
 
 class ActividadResource(ModelResource):
-    imagenes = fields.ToManyField('apps.api.resources.ImagenesResource', 'imagenes_set' ,  related_name='actividad', full=True )
+    #imagenes = fields.ToManyField('apps.api.resources.ImagenesResource', 'imagenes_set' ,  related_name='actividad', full=True )
     class Meta:
         queryset = Actividad.public.all().order_by('-fechaRealizacion')
         limit = 6
         fields = ['nombre', 'lugar', 'fechaRealizacion', 'hora']
+        serializer = Serializer(formats=['json'])
         resource_name = 'act'
 
-class ImagenesResource(ModelResource):
+class ImagenPortadaResource(ModelResource):
     #actividad = fields.ToOneField(ActividadResource, 'actividad').use_in
     class Meta:
         queryset = Imagenes.objects.all()
         fields = ['imagen']
+        limit = 1
+        serializer = Serializer(formats=['json'])
         resource_name = 'img'
-
 
 class CapsulasResource(ModelResource):
     class Meta:
-        queryset = Capsulas.objects.all().filter(fechaPublicacion__range=('2016-01-01', date.today())).order_by('-fechaPublicacion')
-        limit = 1
+        queryset = Capsulas.objects.all()
+        limit = 10
         fields = ['texto', 'imagen']
+        serializer = Serializer(formats=['json'])
         resource_name = 'cap'
+
+class HomeResource(ActividadResource):
+    #imagenes = fields.ToManyField(ImagenPortadaResource, 'imagenes_set' ,  related_name='actividad', full=True )
+    class Meta:
+        queryset = Actividad.public.all().order_by('-fechaRealizacion')
+        limit = 6
+        fields = ['id', 'nombre', 'lugar', 'fechaRealizacion', 'hora']
+        serializer = Serializer(formats=['json'])
+        list_allowed_methods = ['get']
+        authorization = ReadOnlyAuthorization()
+        resource_name = 'home'
+
+    def dehydrate(self, bundle):
+        bundle.data['imagen'] = None
+        try:
+            img = Imagenes.objects.filter(actividad=int(bundle.data['id']))[0]
+            bundle.data['imagen'] = img.imagen
+        except IndexError:
+            pass
+        return bundle
+
+    def alter_list_data_to_serialize(self, request, data):
+        del data['meta']
+        data['capsula'] = {}
+        try:
+            cap = Capsulas.objects.all().filter(fechaPublicacion__range=('2016-01-01', date.today())).order_by('-fechaPublicacion')[0]
+            data['capsula']['texto'] = cap.texto
+            data['capsula']['imagen'] = cap.imagen
+        except IndexError:
+            pass
+        return data
--- APIcasaCultura/apps/api/urls.py
+++ APIcasaCultura/apps/api/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from resources import *
 
-actividad_resource = ActividadResource()
-capsula_resource = CapsulasResource()
-imagenes_resource = ImagenesResource()
+#actividad_resource = ActividadResource()
+#capsula_resource = CapsulasResource()
+#imagenes_resource = ImagenesResource()
+home_resource = HomeResource()
 
 urlpatterns = [
-     url(r'^', include(actividad_resource.urls)),
-     url(r'^', include(capsula_resource.urls)),
-     url(r'^', include(imagenes_resource.urls)),
+#     url(r'^', include(actividad_resource.urls)),
+#     url(r'^', include(capsula_resource.urls)),
+#     url(r'^', include(imagenes_resource.urls)),
+     url(r'^', include(home_resource.urls)),
 ]
