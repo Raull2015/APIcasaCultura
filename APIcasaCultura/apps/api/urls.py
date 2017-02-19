04e4823336d152b0fd34296233eae71e039985ac
Alex Gomez <josuealexander123@hotmail.com>
2017-02-19 01:38:43 -0600

    Update urls.py
    Agregando urls de user, profile, login

--- APIcasaCultura/apps/api/urls.py
+++ APIcasaCultura/apps/api/urls.py
@@ -1,15 +1,21 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from tastypie.api import Api
 from resources import *
 
 #actividad_resource = ActividadResource()
 #capsula_resource = CapsulasResource()
 #imagenes_resource = ImagenesResource()
 home_resource = HomeResource()
+my_api = Api(api_name='cultura')
+my_api.register(UserResource())
+my_api.register(LoginResource())
+my_api.register(ProfileResource())
 
 urlpatterns = [
 #     url(r'^', include(actividad_resource.urls)),
 #     url(r'^', include(capsula_resource.urls)),
 #     url(r'^', include(imagenes_resource.urls)),
      url(r'^', include(home_resource.urls)),
+     url(r'^', include(my_api.urls))
 ]
